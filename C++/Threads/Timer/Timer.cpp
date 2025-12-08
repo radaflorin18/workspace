0e4c8e76214d63a53e0cab382c19c4c7efcf5596
Florin Rada <radaflorin18@gmail.com>
2025-12-08 16:43:45 +0200

    Added comments to explain functionality

--- C++/Threads/Timer/Timer.cpp
+++ C++/Threads/Timer/Timer.cpp
@@ -5,26 +5,27 @@
 #include <future> //includes also thread
 using namespace std::chrono_literals;
 
+//use a template for chrono literals (s, ms, etc...)
 template<class _Rep, class _Period>
 std::future<void> TimerAsync(std::chrono::duration<_Rep, _Period>duration, std::function<void()> callback)
 {
 	return std::async(std::launch::async, [duration, callback]()
 		{
+			std::cout << "Timer started on thread ID: " << std::this_thread::get_id() << std::endl;
 			std::this_thread::sleep_for(duration);
 			callback();
 		});
-
 }
 
 int main()
 {
-
-	auto future = TimerAsync(5s, []() {std::cout << "Timer finished! \n"; });
+	auto my_callback = []() {std::cout << "Timer expired on thread ID: " << std::this_thread::get_id() << std::endl; };
+	auto future = TimerAsync(10s, my_callback);
 
 	//Process the main thread here
 	while (true)
 	{
-		std::cout << "Processing..." << std::endl;
+		std::cout << "Processing thread ID:"<< std::this_thread::get_id() << std::endl;
 		std::this_thread::sleep_for(1s);
 
 		//Optionally terminate program when the timer expires
@@ -33,7 +34,7 @@ int main()
 			break;
 	}
 
-	std::cout << "Finished!" << std::endl;
+	std::cout << "Program finished!" << std::endl;
 
 	return 0;
 }
