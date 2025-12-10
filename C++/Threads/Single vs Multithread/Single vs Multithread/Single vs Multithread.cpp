cbcefde7d6d7cddcefeb139996e0aca1dd9f723f
Florin Rada <radaflorin18@gmail.com>
2025-12-10 16:18:28 +0200

    WIP: passed parameters as references

--- C++/Threads/Single vs Multithread/Single vs Multithread/Single vs Multithread.cpp
+++ C++/Threads/Single vs Multithread/Single vs Multithread/Single vs Multithread.cpp
@@ -1,20 +1,20 @@
 #include <thread>
-#include <stdint.h>
 #include <iostream>
+#include <stdint.h> //for macro UINT64_MAX
 
 using namespace std;
 
 void SumOfEvenNumbers(int max_nb, int &sum)
 {
-	int i = 0;
-	for (i = 0; i <= max_nb; i++)
+	sum = 0;
+	for (int i = 0; i <= max_nb; i++)
 	{
 		if (0 == (i % 2))
 		{
 			sum += i;
 		}
 		//Protection in case of overflow
-		if ((INT64_MAX-sum) <= sum)
+		if ((UINT64_MAX - sum) <= sum)
 		{
 			sum = -1;
 			break;
@@ -24,15 +24,15 @@ void SumOfEvenNumbers(int max_nb, int &sum)
 
 void SumOfOddNumbers(int max_nb, int &sum)
 {
-	int i = 0;
-	for (i = 0; i <= max_nb; i++)
+	sum = 0;
+	for (int i = 0; i <= max_nb; i++)
 	{
 		if (i % 2)
 		{
 			sum += i;
 		}
 		//Protection in case of overflow
-		if ((INT64_MAX - sum) <= sum)
+		if ((UINT64_MAX - sum) <= sum)
 		{
 			sum = -1;
 			break;
@@ -46,21 +46,20 @@ int main()
 	int res_even = 0;
 	int res_odd = 0;
 
-	SumOfEvenNumbers(92681, res_even);
-	SumOfOddNumbers(92680, res_odd);
+	//SumOfEvenNumbers(92681, res_even);
+	//SumOfOddNumbers(92680, res_odd);
 
 
 	// Create a new thread that calls myFunction
-	//thread newThread(SumOfEvenNumbers, 8, res_even);
+	thread Even(SumOfEvenNumbers, 92681, ref(res_even));
+	thread Odd(SumOfOddNumbers, 92680, ref(res_odd));
 
 	// Wait for the new thread to finish execution
-	//newThread.join();
+	Even.join();
+	Odd.join();
 
 	cout << "Result of even operation is: "<< res_even << endl;
 	cout << "Result of odd operation is: " << res_odd << endl;
-
-	return 0;
-
 
 	return 0;
 }
